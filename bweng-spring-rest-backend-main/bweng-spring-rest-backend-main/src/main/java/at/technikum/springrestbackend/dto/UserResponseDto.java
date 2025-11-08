860c0c1de1891ce9fcf5840a6766dce69133ec14
Luca Pal <184681939+luca-pal@users.noreply.github.com>
2025-11-08 18:03:02 +0100

    UserResponseDto created - unstable version (need to update controller)

--- bweng-spring-rest-backend-main/bweng-spring-rest-backend-main/src/main/java/at/technikum/springrestbackend/dto/UserResponseDto.java
+++ bweng-spring-rest-backend-main/bweng-spring-rest-backend-main/src/main/java/at/technikum/springrestbackend/dto/UserResponseDto.java
@@ -1,4 +1,40 @@
 package at.technikum.springrestbackend.dto;
 
+import at.technikum.springrestbackend.entity.Role;
+import at.technikum.springrestbackend.entity.Salutation;
+import at.technikum.springrestbackend.entity.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * Data Transfer Object used to return user information in API responses.
+ * Does not include sensitive fields (ex: password).
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDto {
+
+    private UUID id;
+
+    private Salutation salutation;
+    private String firstName;
+    private String lastName;
+
+    private String address;
+    private String zip;
+    private String city;
+
+    private String email;
+    private String username;
+
+    private Role role;
+    private Status status;
+
+    private Instant createdAt;
+    private Instant updatedAt;
 }
